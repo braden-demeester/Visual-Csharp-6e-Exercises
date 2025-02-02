90558ef09091f69667c001f9979bb486c3df454e
braden-demeester <demeesterbraden@gmail.com>
2025-02-02 15:40:23 -0500

    Add files via upload

---
+++ 13.10/MilesPerGallonCalculator/MilesPerGallonCalculator.cs
@@ -0,0 +1,32 @@
+﻿public class MilesPerGallonCalculator
+{
+    private static double milesDriven, gallonsUsed;
+
+    public static void Main(string[] args)
+    {
+        bool continueLoop = true;
+        while (continueLoop)
+        {
+            try
+            {
+                Console.Write("Miles Driven: ");
+                milesDriven = double.Parse(Console.ReadLine() ?? "");
+
+                Console.Write("Gallons Used: ");
+                gallonsUsed = double.Parse(Console.ReadLine() ?? "");
+
+                Console.WriteLine($"\nMiles Per Gallon: {milesDriven / gallonsUsed:F3}");
+
+                continueLoop = false;
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine($"{fe.Message} Must be a double!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
