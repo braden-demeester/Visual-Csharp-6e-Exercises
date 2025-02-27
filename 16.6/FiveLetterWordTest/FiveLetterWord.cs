f188c3f3ca5a0501c5c58520a8838429eecb0da7
braden-demeester <demeesterbraden@gmail.com>
2025-02-27 15:00:44 -0500

    Add files via upload

---
+++ 16.6/FiveLetterWordTest/FiveLetterWord.cs
@@ -0,0 +1,50 @@
+﻿public class FiveLetterWord
+{
+    private string word = "";
+    public string Word
+    {
+        get => word;
+        private set
+        {
+            // Ensure the value is five characters long
+            if (value.Length != 5)
+                throw new ArgumentException("Word must be five letters long!");
+
+            // Ensure the value is only letters
+            foreach (char c in value) 
+                if (!char.IsLetter(c))
+                    throw new ArgumentException("Word must be composed only of letters!");
+
+            word = value;
+        }
+    }
+
+    /// <summary>
+    /// Create an instance of FiveLetterWord
+    /// </summary>
+    /// <param name="word">The string to be stored as the five-letter word.</param>
+    public FiveLetterWord(string word)
+    {
+        Word = word;
+    }
+
+    /// <summary>
+    /// Produce all three-letter permutations of the word.
+    /// </summary>
+    /// <returns>A string array containing the permutations.</returns>
+    public string[] GetThreeLetterPermutations()
+    {
+        string[] permutations = new string[60]; // 60 = (5 permute 3) = 5 * 4 * 3
+
+        int index = 0; // Used to index the array permutations
+
+        for (int i = 0; i < 5; i++) // Index for first character of a three-letter combination
+            for (int j = 0; j < 5; j++) // Index for second character
+                for (int k = 0; k < 5; k++) // Index for third character
+                    if (i != j && j != k && i != k) // Ensure that the indexes are all unique since we're selecting without replacement
+                        permutations[index++] = new string( // Set permutations[index] to the new string, and incremend index
+                            [ word[i], word[j], word[k] ]); // Create a char[] to convert into a string
+        
+        return permutations;
+    }
+}
---
+++ 16.6/FiveLetterWordTest/FiveLetterWordTest.cs
@@ -0,0 +1,32 @@
+﻿
+public class FiveLetterWordTest
+{
+    private static FiveLetterWord? fiveLetterWord;
+    static void Main(string[] args)
+    {
+        bool continueLoop = true; // Whether to keep looping, i.e. the logic has not yet completed without exception
+        while (continueLoop)
+        {
+            try
+            {
+                Console.Write("Five-letter word: "); // Prompt the user
+                fiveLetterWord = new FiveLetterWord(
+                    Console.ReadLine() ?? ""); // Read user input; use an empty string if ReadLine returns null
+
+                // Print the resulting array, joined with ", "
+                Console.WriteLine(string.Join(", ", fiveLetterWord.GetThreeLetterPermutations()));
+
+
+                continueLoop = false;
+            }
+            catch(ArgumentException ae) // May be thrown when instantiating 'fiveLetterWord'
+            {
+                Console.WriteLine(ae.Message); // Print the exception's Message to inform user
+            }
+            catch(Exception ex) // Catch any other potential exceptions
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
